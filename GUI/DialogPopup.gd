7fb4f7b30a23029934c654ebf5fd2bfdd7d357c5
John Nordberg <john.nordberg@gmail.com>
2021-07-16 22:17:32 +0300

    part14

--- GUI/DialogPopup.gd
+++ GUI/DialogPopup.gd
@@ -4,6 +4,7 @@ extends Popup
 var npc_name setget name_set
 var dialogue setget dialogue_set
 var answers setget answers_set
+var npc
 
 func name_set(new_value):
 	npc_name = new_value
@@ -20,9 +21,35 @@ func answers_set(new_value):
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_process_input(false)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func open():
+	get_tree().paused = true
+	popup()
+	$AnimationPlayer.playback_speed = 60.0 / dialogue.length()
+	$AnimationPlayer.play("ShowDialogue")
+
+
+func close():
+	get_tree().paused = false
+	hide()
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	set_process_input(true)
+
+
+func _input(event):
+	if event is InputEventKey:
+		if event.scancode == KEY_A:
+			set_process_input(false)
+			npc.talk("A")
+		elif event.scancode == KEY_B:
+			set_process_input(false)
+			npc.talk("B")
