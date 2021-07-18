a722815736592723ab9980e98c690624281ca6e0
John Nordberg <john.nordberg@gmail.com>
2021-07-18 13:55:47 +0300

    part19

--- Entities/House/House.gd
+++ Entities/House/House.gd
@@ -19,6 +19,9 @@ func _ready():
 func _on_Area2D_body_entered(body):
 	if body.name == "Player":
 		$Roof.hide()
+	elif body.name.find("Skeleton") >= 0:
+		body.direction = -body.direction
+		body.bounce_countdown = 16
 
 func _on_Area2D_body_exited(body):
 	if body.name == "Player":
---
+++ Scripts/SaveLoadGame.gd
@@ -0,0 +1,18 @@
+extends Node
+
+
+var load_saved_game = false
+
+
+func save():
+	pass
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
